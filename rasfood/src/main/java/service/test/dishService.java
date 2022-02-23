901f0dba4f93630daf56fc943b8a4189a3183781
Alexandre Luís <alexandreluisrigotti@gmail.com>
2022-02-23 13:26:06 -0300

    Exit Transient state -> enter Managed state

---
+++ rasfood/src/main/java/service/test/dishService.java
@@ -0,0 +1,27 @@
+package service.test;
+
+import br.com.rasmoo.restaurant.entity.Dish;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.math.BigDecimal;
+
+
+public class dishService
+{
+    public static void main(String[] args)
+    {
+        Dish risoto = new Dish();
+        risoto.setName("Risoto de frutos do mar.");
+        risoto.setDescription("Risoto acompanhado de lula, polvo e mariscos.");
+        risoto.setAvailable(true);
+        risoto.setValue(BigDecimal.valueOf(88.50));
+
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("rasFood");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.persist(risoto);
+
+    }
+}
